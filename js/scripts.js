e2de3bde9be6486aff67e58b248f176498bbfe9f
Frank Johnson & cleri philiastides <student@epicodus.com>
2015-05-28 13:49:59 -0700

    add parent function toRomanNumeral to use all the functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+var toRomanNumeral = function(number) {
+
+  if (number >= 40) {
+    return fortyToEightyNine(number);
+  } else if (number >= 9) {
+    return nineToThirtyNine(number);
+  } else if (number >= 4) {
+    return fourToEight(number);
+  } else if (number >= 1){
+    return belowFour(number);
+  } else {
+    alert("Please enter a number below 89.");
+  }
+
+};
+
+
 var belowFour = function(number) {
   var romanNumeral = "";
   for(var i = 1; i <= number; i++) {
@@ -8,32 +25,58 @@ var belowFour = function(number) {
 
 var fourToEight = function(number) {
   var romanNumeral = [];
+
   if (number === 4) {
     romanNumeral.push("IV");
+
   } else {
     romanNumeral.push("V");
     var newNum = number - 5;
     romanNumeral.push(belowFour(newNum));
   }
+
   return romanNumeral.join("");
 };
 
-var nineToEighteen = function(number) {
+var nineToThirtyNine = function(number) {
   var romanNumeral = [];
   if (number === 9) {
     romanNumeral.push("IX");
+
   } else {
-    if (number === 10) {
-    romanNumeral.push("X");
-    } else {
+    while (number >= 10) {
       romanNumeral.push("X");
-      var newNum = number - 10;
-        if (newNum < 4) {
-          romanNumeral.push(belowFour(newNum));
-        } else {
-      romanNumeral.push(fourToEight(newNum));
-      }
+      number -= 10;
     }
+    if (number === 9) {
+      romanNumeral.push("IX");
+
+    } else if (number === 5) {
+      romanNumeral.push("V");
+
+    } else if(number < 4) {
+      romanNumeral.push(belowFour(number));
+
+    } else {
+    romanNumeral.push(fourToEight(number));
+    debugger;
+    }
+  }
+  return romanNumeral.join("");
+};
+
+var fortyToEightyNine = function(number) {
+  var romanNumeral = [];
+  if (number === 50) {
+    romanNummeral.push("L");
+  } else if (number < 50) {
+    romanNumeral.push("XL");
+    number -= 40;
+    romanNumeral.push(nineToThirtyNine(number));
+  } else {
+    romanNumeral.push("L");
+    number -= 50;
+    romanNumeral.push(nineToThirtyNine(number)); // problem with 89
   }
   return romanNumeral.join("");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -19,14 +19,57 @@ describe('fourToEight', function() {
   });
 });
 
-describe('nineToEighteen', function() {
-   it("returns the roman numeral for 9", function() {
-     expect(nineToEighteen(9)).to.eql("IX");
-   });
-   it("returns the roman numeral for 10", function() {
-     expect(nineToEighteen(10)).to.eql("X");
-   });
-   it("returns the roman numeral for 15", function() {
-     expect(nineToEighteen(15)).to.eql("XV");
-   });
- });
+describe('nineToThirtyNine', function() {
+  it("returns the roman numeral for 9", function() {
+    expect(nineToThirtyNine(9)).to.eql("IX");
+  });
+  it("returns the roman numeral for 10", function() {
+    expect(nineToThirtyNine(10)).to.eql("X");
+  });
+  it("returns the roman numeral for 15", function() {
+    expect(nineToThirtyNine(15)).to.eql("XV");
+  });
+  it("returns the roman numeral for 27", function() {
+    expect(nineToThirtyNine(27)).to.eql("XXVII");
+  })
+  it("returns the roman numeral for 39", function() {
+    expect(nineToThirtyNine(39)).to.eql("XXXIX");
+  })
+});
+
+describe('fortyToEightyNine', function () {
+  it("returns the roman numeral for 40", function() {
+    expect(fortyToEightyNine(40)).to.equal("XL");
+  });
+  it("returns the roman numeral for 47", function() {
+    expect(fortyToEightyNine(47)).to.equal("XLVII");
+  });
+  it("returns the roman numeral for 52", function() {
+    expect(fortyToEightyNine(52)).to.equal("LII");
+  });
+  it("returns the roman numeral for 86", function() {
+    expect(fortyToEightyNine(86)).to.equal("LXXXVI");
+  });
+  it("returns the roman numeral for 89", function() {
+    expect(fortyToEightyNine(89)).to.equal("LXXXIX");
+  });
+
+});
+
+describe('toRomanNumeral', function() {
+  it("returns the roman numeral of 57", function() {
+    expect(toRomanNumeral(57)).to.equal("LVII");
+  });
+  it("returns the roman numeral for 52", function() {
+    expect(toRomanNumeral(52)).to.equal("LII");
+  });
+  it("returns the roman numeral for 27", function() {
+    expect(toRomanNumeral(27)).to.eql("XXVII");
+  })
+  it("returns the roman number for 6", function() {
+    expect(toRomanNumeral(6)).to.eql("VI");
+  });
+  it("returns the roman numeral for any number below 4", function() {
+    expect(toRomanNumeral(3)).to.eql("III");
+  });
+});
